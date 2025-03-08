af952f6ab694815946ed0e4ca41dd25b040a266a
YuBin <101121572+Have-Been@users.noreply.github.com>
2025-03-08 23:52:38 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 172 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		
+		System.out.println(str.length());
+	}
+}
